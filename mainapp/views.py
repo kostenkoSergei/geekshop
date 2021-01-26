b6fef885c05d4116e847ad87f8c05fc678586327
sergeiKostenko <sdh1985@mail.ru>
2021-01-26 20:20:35 +0300

    filled productcategory table from file

--- mainapp/views.py
+++ mainapp/views.py
@@ -13,11 +13,11 @@ def index(request):
 
 
 def products(request):
-    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    with open(os.path.join(base_dir, 'mainapp/fixtures/products.json'), 'r', encoding='utf-8') as f:
-        products_data = json.load(f)
+    # base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # with open(os.path.join(base_dir, 'mainapp/fixtures/products.json'), 'r', encoding='utf-8') as f:
+    #     products_data = json.load(f)
     context = {
-        'title': 'каталог',
-        'products': products_data
+        # 'title': 'каталог',
+        # 'products': products_data
     }
     return render(request, 'mainapp/products.html', context)
