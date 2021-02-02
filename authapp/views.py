1695da0fa0c1bbebe476921188b13a123dff6ba5
sergeiKostenko <sdh1985@mail.ru>
2021-02-02 18:45:36 +0300

    added profile template

--- authapp/views.py
+++ authapp/views.py
@@ -37,3 +37,7 @@ def register(request):
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('products:index'))
+
+
+def profile(request):
+    return render(request, 'authapp/profile.html')
