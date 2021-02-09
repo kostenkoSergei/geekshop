10a62023d2fd877e02e20be03078fbc6484dc666
sergeiKostenko <sdh1985@mail.ru>
2021-02-09 21:11:01 +0300

    created update and delete users functionality for custom admin

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -6,4 +6,8 @@ app_name = 'adminapp'
 
 urlpatterns = [
     path('', adminapp_views.index, name='index'),
+    path('admin-users-read/', adminapp_views.admin_users_read, name='admin_users_read'),
+    path('admin-users-create/', adminapp_views.admin_users_create, name='admin_users_create'),
+    path('admin-users-update/<int:id>/', adminapp_views.admin_users_update, name='admin_users_update'),
+    path('admin-users-delete/<int:id>/', adminapp_views.admin_users_delete, name='admin_users_delete'),
 ]
