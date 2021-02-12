d24bd9d7c98c6d4325c17ec6fd9dd7300f52b7ed
sergeiKostenko <sdh1985@mail.ru>
2021-02-12 20:07:53 +0300

    added pagination and filtering by category

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,6 +7,6 @@ app_name = 'mainapp'
 urlpatterns = [
     path('', mainapp_views.index, name='index'),
     path('products/', mainapp_views.products, name='products'),
-    path('products/<int:pk>/', mainapp_views.category, name='category'),  # отображает определенную категорию
-    path('product/<int:pk>/', mainapp_views.products, name='product'),  # отображает конкретный продукт
+    path('product/<int:category_id>/', mainapp_views.products, name='product'),
+    path('product/page/<int:page>/', mainapp_views.products, name='page'),
 ]
