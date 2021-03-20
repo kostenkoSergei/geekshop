6841d9e68628989dd8c16c9d73d7bbf791762c56
sergeiKostenko <sdh1985@mail.ru>
2021-03-20 13:13:45 +0300

    rewrote products view using CBV

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -8,7 +8,10 @@ app_name = 'mainapp'
 urlpatterns = [
     # path('', mainapp_views.index, name='index'),
     path('', TemplateView.as_view(template_name='mainapp/index.html'), name='index'),
-    path('products/', mainapp_views.products, name='products'),
-    path('product/<int:category_id>/', mainapp_views.products, name='product'),
-    path('product/page/<int:page>/', mainapp_views.products, name='page'),
+    # path('products/', mainapp_views.products, name='products'),
+    # path('product/<int:category_id>/', mainapp_views.products, name='product'),
+    # path('product/page/<int:page>/', mainapp_views.products, name='page'),
+    path('products/', mainapp_views.ProductView.as_view(), name='products'),
+    path('product/<int:category_id>/', mainapp_views.ProductView.as_view(), name='product'),
+    path('product/page/<int:page>/', mainapp_views.ProductView.as_view(), name='page'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,37 +1,35 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
 from mainapp.models import Product, ProductCategory
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
-# rewrote using view in mainapp/urls.py
-# def index(request):
-#     return render(request, 'mainapp/index.html')
 
 
-# def products(request, category_id=None):
-#     context = {'categories': ProductCategory.objects.all()}
+class ProductView(ListView):
+    model = Product
+    context_object_name = 'products'
+    template_name = 'mainapp/products.html'
+    paginate_by = 3
+
+    def get_queryset(self):
+        if self.request.GET.get('category_id'):
+            return Product.objects.filter(category_id=self.kwargs['category_id'])
+        return Product.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = ProductCategory.objects.all()
+        return context
+
+# def products(request, category_id=None, page=1):
 #     if category_id:
 #         products = Product.objects.filter(category_id=category_id)
 #     else:
 #         products = Product.objects.all()
-#     context.update({'products': products})
+#     per_page = 3
+#     paginator = Paginator(products.order_by('-price'), per_page)
+#     products_paginator = paginator.page(page)
+#     context = {
+#         'categories': ProductCategory.objects.all(),
+#         'products': products_paginator
+#     }
 #     return render(request, 'mainapp/products.html', context)
-
-def products(request, category_id=None, page=1):
-    if category_id:
-        products = Product.objects.filter(category_id=category_id)
-    else:
-        products = Product.objects.all()
-    per_page = 3
-    paginator = Paginator(products.order_by('-price'), per_page)
-    products_paginator = paginator.page(page)
-    context = {
-        'categories': ProductCategory.objects.all(),
-        'products': products_paginator
-    }
-    return render(request, 'mainapp/products.html', context)
-    # try:
-    #     products_paginator = paginator.page(page)
-    # except PageNotAnInteger:
-    #     products_paginator = paginator.page(1)
-    # except EmptyPage:
-    #     products_paginator = paginator.page(paginator.num_pages)
