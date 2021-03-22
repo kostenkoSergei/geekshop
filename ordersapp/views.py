4d155b455671e32e9eabfe437cd5244199692742
sergeiKostenko <sdh1985@mail.ru>
2021-03-22 19:26:14 +0300

    changed OrderItemsCreate class as teacher

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -25,18 +25,27 @@ class OrderItemsCreate(CreateView):
     model = Order
     fields = []
     success_url = reverse_lazy('ordersapp:orders_list')
-
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
         OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=1)
-
         if self.request.POST:
             formset = OrderFormSet(self.request.POST)
         else:
-            formset = OrderFormSet()
+            # formset = OrderFormSet()
+            basket_items = Basket.objects.filter(user=self.request.user)
+            if len(basket_items):
+                OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=len(basket_items))
+                formset = OrderFormSet()
+                for num, form in enumerate(formset.forms):
+                    form.initial['product'] = basket_items[num].product
+                    form.initial['quantity'] = basket_items[num].quantity
+                basket_items.delete()
+            else:
+                formset = OrderFormSet()
 
         data['orderitems'] = formset
         return data
+
 
     def form_valid(self, form):
         context = self.get_context_data()
