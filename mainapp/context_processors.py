5d29568cdd6f99b3515da670aa5ad5ba19ae55b9
sergeiKostenko <sdh1985@mail.ru>
2021-03-24 08:28:13 +0300

    added basket counter in context processor

--- mainapp/context_processors.py
+++ mainapp/context_processors.py
@@ -6,7 +6,9 @@ def user_basket(request):
 
     if request.user.is_authenticated:
         user_basket = Basket.objects.filter(user=request.user)
+        user_basket_counter = Basket.objects.filter(user=request.user).count()
 
     return {
         'user_basket': user_basket,
+        'user_basket_counter': user_basket_counter,
     }
